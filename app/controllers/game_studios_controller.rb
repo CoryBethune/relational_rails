ff6cfded9acf545f3b4da17ad457a3b0cc715956
Cory Bethune <corybbethune@gmail.com>
2022-03-30 17:11:03 -0700

    user story 10 tests passing

--- app/controllers/game_studios_controller.rb
+++ app/controllers/game_studios_controller.rb
@@ -5,6 +5,7 @@ class GameStudiosController < ApplicationController
   end
 
   def show
+    binding.pry
     @studio = GameStudio.find(params[:id])
     @game_count = @studio.count_of_games
   end
--- spec/features/links_spec.rb
+++ spec/features/links_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "links" do
+
+  before (:each) do
+    @studio1 = GameStudio.create!(name: 'SquareSoft', still_active: true, game_count: 10)
+    @studio2 = GameStudio.create!(name: 'Bethesda', still_active: false, game_count: 20)
+    @game = @studio1.games.create!(name: 'FFVII', still_in_production: true, hours_of_gameplay: 100)
+    @game2 = Game.create!(name: 'Blah', still_in_production: false, hours_of_gameplay: 000)
+  end
+
   it 'links to the games index' do
     visit '/game_studios'
 
@@ -15,6 +23,14 @@ RSpec.describe "links" do
     click_link('Game Studio Index')
 
     expect(current_path).to eq('/game_studios')
+  end
+
+  it 'links to the show games by parent id page' do
+    visit "/game_studios/#{@studio1.id}"
+
+    click_link('Games of the Studio')
+
+    expect(current_path).to eq("/game_studios/#{@studio1.id}/games")
   end
 
 end
